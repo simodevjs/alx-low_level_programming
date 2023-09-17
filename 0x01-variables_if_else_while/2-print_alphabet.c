48f5266f7f62747f5e22e239d8274febe2262c63
System Administrator <root@MohamedMAHLAs-MacBook-Pro-4.local>
2023-09-17 05:32:32 +0100

    222

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,18 @@
-#include <stdio.h> 
- /** 
-  * main - prints the alphabet in lowercase using putchar. 
-  * 
-  * Return: Always 0. 
-  */ 
- int main(void) 
- { 
-         char alp[27] = "abcdefghijklmnopqrstuvwxyz"; 
-         int i; 
-  
-         for (i = 0; i < 26; i++) 
-         { 
-                 putchar(alp[i]); 
-         } 
-         putchar('\n'); 
-         return (0); 
- }
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char alp[] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+		putchar('\n');
+	}
+
+	return (0);
+}
