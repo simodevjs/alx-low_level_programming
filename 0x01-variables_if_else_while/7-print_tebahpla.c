c7caf4c2762e2a820b0818c688d26bdb96da9cdd
System Administrator <root@MohamedMAHLAs-MacBook-Pro-4.local>
2023-09-17 23:48:20 +0100

    222

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,21 +1,18 @@
-#include <stdio.h> 
- /** 
-  * main - Entry Point Of the Programme. 
-  * 
-  * Description: print alphabets in reverse 
-  * 
-  * Return: Always 0 (Success) 
-  */ 
- int main(void) 
- { 
-         char revalp[27] = "zyxwvutsrqponmlkjihgfedcba"; 
-  
-         int i; 
-  
-         for (i = 0; i < 26; i++) 
-         { 
-                 putchar(revalp[i]); 
-         } 
-         putchar('\n'); 
-         return (0); 
- }
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char alp[] = "zyxwvutsrqponmlkjihgfedcba";
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+
+	putchar('\n');
+	return (0);
+}
