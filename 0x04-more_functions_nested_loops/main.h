e095638a1be2f9c04cf93fa44ac2da2ce9a8af31
System Administrator <root@MohamedMAHLAs-MacBook-Pro-4.local>
2023-09-22 02:00:02 +0100

    222

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,7 +3,7 @@
 
 int _putchar(char c);
 int _isupper(int c);
-/*int _isdigit(int c);
+int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
@@ -12,5 +12,5 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-*/
+
 #endif
