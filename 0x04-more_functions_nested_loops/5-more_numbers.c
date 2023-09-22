15789cfac72c390f1c5112725dfb9be900868ba4
System Administrator <root@MohamedMAHLAs-MacBook-Pro-4.local>
2023-09-22 02:28:21 +0100

    222

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - jdnede;
+ * print_to_14 - prints numbers [0-14]
+ * Return: dzidizdzd
+ */
+void more_numbers(void)
+{
+	int m = 0;
+	for ( m = 0; m < 14 ; m++)
+	{
+	        int n = 0;
+			while (n <= 14)
+			{
+				if (n > 10)
+					_putchar(n / 10 + 48);
+				_putchar(n % 10 + 48);
+				n++;
+			}
+			_putchar('\n');
+	}
+}
